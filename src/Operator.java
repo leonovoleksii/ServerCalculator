ef80a7858f6070fc132d263fd11c3bb71f3be3d4
oleksiileonov <leonov.oleksii73@gmail.com>
2019-07-04 17:06:36 +0300

    Create class Operator

---
+++ src/Operator.java
@@ -0,0 +1,37 @@
+import java.lang.Comparable;
+import java.lang.IllegalArgumentException;
+
+public class Operator implements Comparable<Operator> {
+    private char value;
+    private byte priority;
+
+    public Operator(char value) {
+        this.value = value;
+        if (value == '+' || value == '-') priority = 0;
+        else if (value == '*' || value == '/' || value == '%') priority = 1;
+        else if (value == '(' || value == ')') priority = 2;
+        else throw new IllegalArgumentException("Wrong operator");
+    }
+
+    @Override
+    public int compareTo(Operator op) {
+        if (this.priority < op.priority) return -1;
+        else if (this.priority > op.priority) return 1;
+        else return 0;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public byte getPriority() {
+        return priority;
+    }
+
+    // unit testing
+    public static void main(String[] args) {
+        Operator op1 = new Operator('+');
+        Operator op2 = new Operator('*');
+        System.out.println(op1.compareTo(op2));
+    }
+}
