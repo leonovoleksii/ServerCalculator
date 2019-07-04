5bf0ce14797f3d233f918871057ce07c71b7840d
oleksiileonov <leonov.oleksii73@gmail.com>
2019-07-04 18:37:02 +0300

    Write method isOperand()

--- src/Operand.java
+++ src/Operand.java
@@ -16,10 +16,27 @@ public class Operand {
         return value;
     }
 
+    public static boolean isOperand(String s) {
+        int cntDot = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') cntDot++;
+            else if (!(s.charAt(i) >= '0' && s.charAt(i) <= '9')) return false;
+        }
+        if (cntDot > 1 || cntDot == 1 && s.length() == 1 || s.length() == 0) return false;
+        return true;
+    }
+
     // unit tests
     public static void main(String[] args) {
         String expr = new String(".3");
         Operand op = new Operand(expr);
         System.out.println(op.getValue());
+        System.out.println(isOperand("123"));
+        System.out.println(isOperand("12.3"));
+        System.out.println(isOperand("123."));
+        System.out.println(isOperand(".123"));
+        System.out.println(isOperand("123,"));
+        System.out.println(isOperand("."));
+        System.out.println(isOperand(""));
     }
 }
