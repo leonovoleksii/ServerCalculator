dae64fb52cfcff65a741dd7fdc7cfd4bf45f58c8
oleksiileonov <leonov.oleksii73@gmail.com>
2019-07-08 16:23:20 +0300

    Move realization of calculation form main to new method calculate

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.Stack;
 
 public class Calculator {
@@ -11,13 +12,38 @@ public class Calculator {
         writer = new BufferedWriter(out);
     }
 
+    public void calculate(String expression) throws IOException {
+        Stack<Operand> operands = new Stack<>();
+        expression = removeSpaces(expression);
+        if (!checkValidity(expression)) {
+            return;
+        }
+        converter = new InfixToPostfixConverter(expression);
+        converter.convert();
+        for (String s : converter.getPostfixExpression()) {
+            if (Operator.isOperator(s)) {
+                Operand second = operands.pop();
+                Operand first = operands.pop();
+                operands.push((new Operator(s)).apply(first, second));
+            } else {
+                operands.push(new Operand(s));
+            }
+        }
+        if (operands.empty()) {
+            writer.write(0 + "\n");
+        } else {
+            writer.write(operands.pop().getValue().toString() + "\n");
+        }
+        writer.flush();
+    }
+
     // returns true iff char c can be used in operand notation
     private static boolean isOperandSym(char c) {
         return c >= '0' && c <= '9' || c == '.';
     }
 
     // returns true iff operands and operators in the expression are valid
-    private static boolean checkOps(String expr) throws IOException {
+    private boolean checkOps(String expr) throws IOException {
         // build a string without parentheses
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < expr.length(); i++) {
@@ -59,7 +85,7 @@ public class Calculator {
     }
 
     // returns true iff the sequence of parentheses in the expression is right
-    private static boolean checkParen(String expr) throws IOException {
+    private boolean checkParen(String expr) throws IOException {
         Stack<Character> parentheses = new Stack<>();
         for (int i = 0; i < expr.length(); i++) {
             char cur = expr.charAt(i);
@@ -105,12 +131,12 @@ public class Calculator {
     }
 
     // returns true iff the expression is valid
-    private static boolean checkValidity(String expr) throws IOException {
+    private boolean checkValidity(String expr) throws IOException {
         return checkOps(expr) && checkParen(expr);
     }
 
     // removes spaces from expression
-    private static String removeSpaces(String expression) {
+    private String removeSpaces(String expression) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < expression.length(); i++) {
             if (!Character.isSpaceChar(expression.charAt(i)))
@@ -119,33 +145,9 @@ public class Calculator {
         return sb.toString();
     }
 
-
+    // unit testing
     public static void main(String[] args) throws IOException {
-        reader = new BufferedReader(new InputStreamReader(System.in));
-        writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        String expression;
-        Stack<Operand> operands = new Stack<>();
-        while (true) {
-            writer.write("Print the expression you want to calculate (print exit or ^C to exit):\n");
-            writer.flush();
-            expression = removeSpaces(reader.readLine());
-            if (expression.equals("exit")) break;
-            if (!checkValidity(expression)) {
-                continue;
-            }
-            converter = new InfixToPostfixConverter(expression);
-            converter.convert();
-            for (String s : converter.getPostfixExpression()) {
-                if (Operator.isOperator(s)) {
-                    Operand second = operands.pop();
-                    Operand first = operands.pop();
-                    operands.push((new Operator(s)).apply(first, second));
-                } else {
-                    operands.push(new Operand(s));
-                }
-            }
-            writer.write(operands.pop().getValue().toString() + "\n");
-            writer.flush();
-        }
+        Calculator calculator = new Calculator(new InputStreamReader(System.in), new OutputStreamWriter(System.out));
+        calculator.calculate("2 + 2");
     }
 }
